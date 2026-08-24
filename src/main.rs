39b2178f97101b36d9df65899f0dd8b201bb1400
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-24 03:41:52 +0200

    Add applet entry point

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+mod app;
+mod wayland;
+
+fn main() -> cosmic::iced::Result {
+    tracing_subscriber::fmt::init();
+    let _ = tracing_log::LogTracer::init();
+    app::run()
+}
